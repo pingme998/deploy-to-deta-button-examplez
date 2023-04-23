d60b95c6b6a70169cca56f4922d882e18bd9ccc9
pingme998 <77038591+pingme998@users.noreply.github.com>
2023-04-23 09:53:18 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,21 @@
 import os
+import requests
 from fastapi import FastAPI
 
 app = FastAPI()
 
+def ping_website(url):
+    try:
+        response = requests.get(url)
+        if response.status_code == 200:
+            print(f"Pinged {url} successfully!")
+        else:
+            print(f"Received a {response.status_code} status code when pinging {url}")
+    except Exception as e:
+        print(f"Encountered an error while pinging {url}: {e}")
+
 @app.get("/")
 async def index():
-	name = os.getenv("NAME", "world")
-	return f"hello {name}!"
+    ping_website("https://localhost:8089/get")
+    name = os.getenv("NAME", "world")
+    return f"hello {name}!"
